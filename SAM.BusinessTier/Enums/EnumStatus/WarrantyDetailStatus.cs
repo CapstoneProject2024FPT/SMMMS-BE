c8e4473e76219eec10c3708c7360a8374435371f
Nguyễn Vinh <vinhnguyenk147@gmail.com>
2024-06-25 15:52:38 +0700

    update

--- SAM.BusinessTier/Enums/EnumStatus/WarrantyDetailStatus.cs
+++ SAM.BusinessTier/Enums/EnumStatus/WarrantyDetailStatus.cs
@@ -10,7 +10,7 @@ namespace SAM.BusinessTier.Enums.EnumStatus
     {
         AwaitingAssignment,  // Đang đợi giao cho nhân viên
         Process,          // Đang tiến hành (chuẩn bị tới nhà)
-        OnSite,              // Đang sửa
+        ScheduledRepair,              //  đặt lịch sửa chữa tiếp theo
         Repairing,            // Đang sửa chữa
         Complete,            // Hoàn thành
         Cancel
--- SAM.BusinessTier/Enums/EnumTypes/WarrantyTypes.cs
+++ SAM.BusinessTier/Enums/EnumTypes/WarrantyTypes.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SAM.BusinessTier.Enums.EnumStatus
+namespace SAM.BusinessTier.Enums.EnumTypes
 {
     public enum WarrantyTypes
     {
