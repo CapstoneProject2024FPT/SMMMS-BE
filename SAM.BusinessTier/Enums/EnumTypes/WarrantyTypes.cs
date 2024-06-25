fe58158de71b110d8435736a9cd395558bc8cc61
Nguyễn Vinh <vinhnguyenk147@gmail.com>
2024-06-25 15:40:01 +0700

    add type warranty

--- SAM.BusinessTier/Enums/EnumTypes/WarrantyTypes.cs
+++ SAM.BusinessTier/Enums/EnumTypes/WarrantyTypes.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SAM.BusinessTier.Enums.EnumStatus
 {
-    public enum WarrantyStatus
+    public enum WarrantyTypes
     {
         NoWarranty,  // Không bảo hành
         Limited,      // Bảo hành có giới hạn
