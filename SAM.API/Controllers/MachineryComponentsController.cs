f14e5c6924641b2f1908cfefe7202fa7ec386900
Nguyễn Vinh <vinhnguyenk147@gmail.com>
2024-06-26 15:12:34 +0700

    add machinery component service

--- SAM.API/Controllers/MachineryComponentsController.cs
+++ SAM.API/Controllers/MachineryComponentsController.cs
@@ -1,6 +1,9 @@
 ﻿using DentalLabManagement.API.Controllers;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using SAM.BusinessTier.Constants;
+using SAM.BusinessTier.Payload.Discount;
+using SAM.BusinessTier.Payload.MachineryComponent;
 using SAM.BusinessTier.Services.Implements;
 using SAM.BusinessTier.Services.Interfaces;
 
@@ -13,6 +16,38 @@ namespace SAM.API.Controllers
             public MachineryComponentsController(ILogger<MachineryComponentsController> logger, IMachineryComponentService machineryComponentService) : base(logger)
         {
             _machineryComponentService = machineryComponentService;
+        }
+        [HttpPost(ApiEndPointConstant.MachineryComponent.MachineryComponentsEndPoint)]
+        public async Task<IActionResult> CreateNewMachineryComponents(CreateNewMachineryComponentRequest createNewMachineryComponentRequest)
+        {
+            var response = await _machineryComponentService.CreateNewMachineryComponents(createNewMachineryComponentRequest);
+            return Ok(response);
+        }
+        [HttpGet(ApiEndPointConstant.MachineryComponent.MachineryComponentsEndPoint)]
+        public async Task<IActionResult> GetMachineryComponentList([FromQuery] MachineryComponentFilter filter)
+        {
+            var response = await _machineryComponentService.GetMachineryComponentList(filter);
+            return Ok(response);
+        }
+        [HttpGet(ApiEndPointConstant.MachineryComponent.MachineryComponentEndPoint)]
+        public async Task<IActionResult> GetMachineryComponentById(Guid id)
+        {
+            var response = await _machineryComponentService.GetMachineryComponentById(id);
+            return Ok(response);
+        }
+        [HttpPut(ApiEndPointConstant.MachineryComponent.MachineryComponentEndPoint)]
+        public async Task<IActionResult> UpdateMachineryComponent(Guid id, UpdateMachineryComponentRequest updateMachineryComponentRequest)
+        {
+            var isSuccessful = await _machineryComponentService.UpdateMachineryComponent(id, updateMachineryComponentRequest);
+            if (!isSuccessful) return Ok(MessageConstant.MachineryComponents.UpdateMachineryComponentsFailedMessage);
+            return Ok(MessageConstant.MachineryComponents.UpdateMachineryComponentsSuccessMessage);
+        }
+        [HttpDelete(ApiEndPointConstant.MachineryComponent.MachineryComponentEndPoint)]
+        public async Task<IActionResult> RemoveMachineryComponentStatus(Guid id)
+        {
+            var isSuccessful = await _machineryComponentService.RemoveMachineryComponentStatus(id);
+            if (!isSuccessful) return Ok(MessageConstant.MachineryComponents.UpdateMachineryComponentsFailedMessage);
+            return Ok(MessageConstant.MachineryComponents.UpdateMachineryComponentsSuccessMessage);
         }
     }
 }
