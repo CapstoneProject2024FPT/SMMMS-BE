1d4b714960dfc7548226c0504811600137c4d915
Nguyễn Vinh <vinhnguyenk147@gmail.com>
2024-07-04 22:46:13 +0700

    Fix Endpoint

--- SAM.BusinessTier/Constants/ApiEndPointConstant.cs
+++ SAM.BusinessTier/Constants/ApiEndPointConstant.cs
@@ -21,8 +21,8 @@ public static class ApiEndPointConstant
         public const string UserEndPointMember = UsersEndPoint + "/{id}";
         public const string UserToRankEndPoint = UsersEndPoint + "/{id}/addRank";
         public const string UserEndPointChangePassword = UsersEndPoint + "/{id}/password";
-        public const string StaffEndPoint = ApiEndpoint + "/resigter/staff";
-        public const string AccountEndPoint = ApiEndpoint + "/resigter/customer";
+        public const string StaffEndPoint = ApiEndpoint + "/register/staff";
+        public const string AccountEndPoint = ApiEndpoint + "/register/customer";
 
     }
 
