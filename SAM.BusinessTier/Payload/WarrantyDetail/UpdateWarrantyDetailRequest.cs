18dd4c57ae20ac56f2e972197c27529838df4653
Nguyễn Vinh <vinhnguyenk147@gmail.com>
2024-07-10 22:34:01 +0700

    Update WarrantyDetail service

--- SAM.BusinessTier/Payload/WarrantyDetail/UpdateWarrantyDetailRequest.cs
+++ SAM.BusinessTier/Payload/WarrantyDetail/UpdateWarrantyDetailRequest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SAM.BusinessTier.Enums.EnumStatus;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,17 @@ namespace SAM.BusinessTier.Payload.WarrantyDetail
 {
     public class UpdateWarrantyDetailRequest
     {
+        public DateTime? CompletionDate { get; set; }
+
+        public WarrantyDetailStatus? Status { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? Comments { get; set; }
+
+        public DateTime? NextMaintenanceDate { get; set; }
+
+        public Guid? AccountId { get; set; }
+
     }
 }
--- SAM.BusinessTier/Services/Implements/WarrantyDetailService.cs
+++ SAM.BusinessTier/Services/Implements/WarrantyDetailService.cs
@@ -1,9 +1,11 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using SAM.BusinessTier.Constants;
 using SAM.BusinessTier.Payload.Brand;
 using SAM.BusinessTier.Payload.WarrantyDetail;
 using SAM.BusinessTier.Services.Interfaces;
+using SAM.BusinessTier.Utils;
 using SAM.DataTier.Models;
 using SAM.DataTier.Repository.Interfaces;
 using System;
@@ -40,9 +42,35 @@ namespace SAM.BusinessTier.Services.Implements
             throw new NotImplementedException();
         }
 
-        public Task<bool> UpdateWarrantyDetail(Guid id, UpdateWarrantyDetailRequest updateWarrantyDetailRequest)
+        public async Task<bool> UpdateWarrantyDetail(Guid id, UpdateWarrantyDetailRequest updateDetailRequest)
         {
-            throw new NotImplementedException();
+            if (id == Guid.Empty) throw new BadHttpRequestException(MessageConstant.WarrantyDetail.EmptyWarrantyDetailIdMessage);
+
+            WarrantyDetail warrantyDetail = await _unitOfWork.GetRepository<WarrantyDetail>().SingleOrDefaultAsync(
+                predicate: x => x.Id.Equals(id))
+            ?? throw new BadHttpRequestException(MessageConstant.WarrantyDetail.WarrantyDetailNotFoundMessage);
+
+            // Update warranty detail properties
+            warrantyDetail.CompletionDate = updateDetailRequest.CompletionDate.HasValue ? updateDetailRequest.CompletionDate.Value : warrantyDetail.CompletionDate;
+            warrantyDetail.Status = updateDetailRequest.Status.GetDescriptionFromEnum();
+            warrantyDetail.Description = !string.IsNullOrEmpty(updateDetailRequest.Description) ? updateDetailRequest.Description : warrantyDetail.Description;
+            warrantyDetail.Comments = !string.IsNullOrEmpty(updateDetailRequest.Comments) ? updateDetailRequest.Comments : warrantyDetail.Comments;
+            warrantyDetail.NextMaintenanceDate = updateDetailRequest.NextMaintenanceDate.HasValue ? updateDetailRequest.NextMaintenanceDate.Value : warrantyDetail.NextMaintenanceDate;
+
+            // Update associated account if AccountId is provided
+            if (updateDetailRequest.AccountId.HasValue)
+            {
+                Account account = await _unitOfWork.GetRepository<Account>().SingleOrDefaultAsync(
+                    predicate: x => x.Id.Equals(updateDetailRequest.AccountId.Value))
+                ?? throw new BadHttpRequestException(MessageConstant.Account.NotFoundFailedMessage);
+
+                warrantyDetail.Account = account;
+            }
+
+            _unitOfWork.GetRepository<WarrantyDetail>().UpdateAsync(warrantyDetail);
+            bool isSuccess = await _unitOfWork.CommitAsync() > 0;
+            return isSuccess;
         }
+
     }
 }
