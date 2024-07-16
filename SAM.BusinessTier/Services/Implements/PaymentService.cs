7da980ddf3d7115dfab9bb476c7e9ea416aba6bd
Nguyễn Vinh <vinhnguyenk147@gmail.com>
2024-07-16 15:21:33 +0700

    fix data type in amount

--- SAM.BusinessTier/Services/Implements/PaymentService.cs
+++ SAM.BusinessTier/Services/Implements/PaymentService.cs
@@ -41,7 +41,7 @@ namespace SAM.BusinessTier.Services.Implements
             pay.AddRequestData("vnp_Version", _configuration["Vnpay:Version"]);
             pay.AddRequestData("vnp_TmnCode", _configuration["Vnpay:TmnCode"]);
             pay.AddRequestData("vnp_Command", _configuration["Vnpay:Command"]);
-            pay.AddRequestData("vnp_Amount", ((int)request.Amount * 100).ToString());
+            pay.AddRequestData("vnp_Amount", ((double)request.Amount).ToString());
             pay.AddRequestData("vnp_CreateDate", currentTimeStamp);
             pay.AddRequestData("vnp_CurrCode", _configuration["Vnpay:CurrCode"]);
             pay.AddRequestData("vnp_IpAddr", pay.GetIpAddress(_httpContextAccessor.HttpContext));
