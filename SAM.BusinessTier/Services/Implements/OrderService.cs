82dc494d6590f3e7820687633f2bcd495c4dabcf
Nguyễn Vinh <vinhnguyenk147@gmail.com>
2024-08-15 17:17:02 +0700

    Update OrderService.cs

--- SAM.BusinessTier/Services/Implements/OrderService.cs
+++ SAM.BusinessTier/Services/Implements/OrderService.cs
@@ -104,26 +104,27 @@ namespace SAM.BusinessTier.Services.Implements
                     };
 
                     orderDetails.Add(orderDetail);
-                    totalAmount += orderDetail.TotalAmount ?? 0;
+                    totalAmount += orderDetail.FinalPrice ?? 0;
                 }
             }
 
-            //// Tính toán tổng số tiền và giảm giá theo hạng tài khoản
-            //var accountRank = await _unitOfWork.GetRepository<AccountRank>()
-            //    .SingleOrDefaultAsync(predicate: ar => ar.AccountId == account.Id);
+            // Tính toán tổng số tiền và giảm giá theo hạng tài khoản
+            var accountRank = await _unitOfWork.GetRepository<AccountRank>()
+                .SingleOrDefaultAsync(predicate: ar => ar.AccountId == account.Id);
 
-            //if (accountRank != null)
-            //{
-            //    var rank = await _unitOfWork.GetRepository<Rank>()
-            //        .SingleOrDefaultAsync(predicate: r => r.Id == accountRank.RankId);
+            if (accountRank != null)
+            {
+                var rank = await _unitOfWork.GetRepository<Rank>()
+                    .SingleOrDefaultAsync(predicate: r => r.Id == accountRank.RankId);
 
-            //        double rankDiscount = (rank.Value.Value / 100.0) * totalAmount;
-            //        newOrder.FinalAmount = totalAmount - rankDiscount;
+                double rankDiscount = (rank.Value.Value / 100.0) * totalAmount;
+                newOrder.FinalAmount = totalAmount - rankDiscount;
 
-            //}else
-            //{
-            //    newOrder.FinalAmount = totalAmount;
-            //}
+            }
+            else
+            {
+                newOrder.FinalAmount = totalAmount;
+            }
 
             await _unitOfWork.GetRepository<Order>().InsertAsync(newOrder);
             await _unitOfWork.GetRepository<OrderDetail>().InsertRangeAsync(orderDetails);
